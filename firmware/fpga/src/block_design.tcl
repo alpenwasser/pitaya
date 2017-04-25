c95f8a7ea6757e0cd121b9376cd4fb746fe0b81e
Noah Huesser <yatekii@yatekii.ch>
2017-04-25 11:07:50 +0200

    adjusted current BD generator script to fit working one; minor errors still

--- firmware/fpga/src/block_design.tcl
+++ firmware/fpga/src/block_design.tcl
@@ -23,7 +23,7 @@ create_bd_design system
 # ====================================================================================
 # IP repos
 set currrent_dir [get_property DIRECTORY [current_project]]
-set_property  ip_repo_paths  {build/cores $current_dir/../../../../logger} [current_project]
+set_property  ip_repo_paths  {build/cores zynq_logger/build/fpga} [current_project]
 update_ip_catalog
 
 # Create basic Red Pitaya Block Design
@@ -46,26 +46,40 @@ source $project_name/basic_red_pitaya_bd.tcl
 # ====================================================================================
 # RTL modules
 
-# System rocessor Reset
+# System processor Reset
 create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 system_rst
 
-# AXI Interconnect
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 interconnect
-set_property -dict [list CONFIG.NUM_MI {2}] [get_bd_cells interconnect]
-set_property -dict [list CONFIG.NUM_SI {2}] [get_bd_cells interconnect]
+# Blinking LED & slow clock
+create_bd_cell -type ip -vlnv xilinx.com:ip:c_counter_binary:12.0 2HzCnt
+set_property -dict [list CONFIG.Output_Width {32}] [get_bd_cells 2HzCnt]
+create_bd_cell -type ip -vlnv xilinx.com:ip:xlslice:1.0 2HzSlc
+set_property -dict [list CONFIG.DIN_TO {26}] [get_bd_cells 2HzSlc]
+set_property -dict [list CONFIG.DIN_FROM {26}] [get_bd_cells 2HzSlc]
+set_property -dict [list CONFIG.DOUT_WIDTH {1}] [get_bd_cells 2HzSlc]
+
+# AXI Protocol Converters
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_protocol_converter:2.1 M2Sconverter
+set_property -dict [list CONFIG.MI_PROTOCOL.VALUE_SRC USER CONFIG.SI_PROTOCOL.VALUE_SRC USER] [get_bd_cells M2Sconverter]
+set_property -dict [list CONFIG.MI_PROTOCOL.VALUE_SRC USER CONFIG.SI_PROTOCOL.VALUE_SRC USER] [get_bd_cells M2Sconverter]
+set_property -dict [list CONFIG.SI_PROTOCOL {AXI4LITE}] [get_bd_cells M2Sconverter]
+set_property -dict [list CONFIG.MI_PROTOCOL {AXI3}] [get_bd_cells M2Sconverter]
+set_property -dict [list CONFIG.TRANSLATION_MODE {2}] [get_bd_cells M2Sconverter]
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_protocol_converter:2.1 S2Mconverter
 
 # Module to convert the ADC AXI Stream data to the data lanes the Logger Core requires
-startgroup
 create_bd_cell -type ip -vlnv noah-huesser:user:axis_to_data_lanes:1.0 axis2lanes
-endgroup
 
 # The Logger module
-startgroup
-create_bd_cell -type ip -vlnv noah.huesser:user:zynq_logger:1.1 logger
-endgroup
+create_bd_cell -type ip -vlnv bastli:user:zynq_logger:1.1 logger
+assign_bd_address [get_bd_addr_segs {logger/S0/Reg }]
 
 # ====================================================================================
 # Connections
+
+# Blinking LED
+connect_bd_net [get_bd_pins 2HzCnt/Q] [get_bd_pins 2HzSlc/Din]
+connect_bd_net [get_bd_ports led_o] [get_bd_pins 2HzSlc/Dout]
+connect_bd_net [get_bd_pins 2HzCnt/CLK] [get_bd_pins ps/FCLK_CLK0]
 
 # Connect ADC to AXIS to Data Lanes
 connect_bd_intf_net [get_bd_intf_pins adc/M_AXIS] [get_bd_intf_pins axis2lanes/SI]
@@ -85,26 +99,30 @@ connect_bd_net [get_bd_pins logger/Data0xDI] [get_bd_pins axis2lanes/Data0xDO]
 connect_bd_net [get_bd_pins axis2lanes/Data1xDO] [get_bd_pins logger/Data1xDI]
 connect_bd_net [get_bd_pins axis2lanes/DataStrobexDO] [get_bd_pins logger/DataStrobexSI]
 
-# ZYNQ7 Processing System to Interconnect
-connect_bd_intf_net [get_bd_intf_pins ps/S_AXI_HP0] -boundary_type upper [get_bd_intf_pins interconnect/M00_AXI]
-connect_bd_intf_net [get_bd_intf_pins ps/M_AXI_GP0] -boundary_type upper [get_bd_intf_pins interconnect/S01_AXI]
-
 # ZYNQ7 Processing System to Zynq Logger
 connect_bd_net [get_bd_pins ps/Core0_nIRQ] [get_bd_pins logger/IRQxSO]
 
-# AXI Interconnect Clocks
-connect_bd_net [get_bd_pins interconnect/ACLK] [get_bd_pins ps/FCLK_CLK0]
-connect_bd_net [get_bd_pins interconnect/S00_ACLK] [get_bd_pins ps/FCLK_CLK0]
-connect_bd_net [get_bd_pins interconnect/M00_ACLK] [get_bd_pins ps/FCLK_CLK0]
+# Reset to Logger
+connect_bd_net [get_bd_pins system_rst/peripheral_aresetn] [get_bd_pins logger/MAxiRstxRBI]
+connect_bd_net [get_bd_pins system_rst/peripheral_aresetn] [get_bd_pins logger/SAxiAResetxRBI]
 
-# AXI Interconnect Resets
-connect_bd_net [get_bd_pins interconnect/ARESETN] [get_bd_pins system_rst/interconnect_aresetn]
-connect_bd_net [get_bd_pins interconnect/S00_ARESETN] [get_bd_pins system_rst/peripheral_aresetn]
-connect_bd_net [get_bd_pins interconnect/M00_ARESETN] [get_bd_pins system_rst/peripheral_aresetn]
+# Reset to axis2lanes
+connect_bd_net [get_bd_pins system_rst/peripheral_aresetn] [get_bd_pins axis2lanes/DataRstxRBI]
 
-# Processing System to System Reset
-connect_bd_net [get_bd_pins ps/FCLK_RESET0_N] [get_bd_pins system_rst/aux_reset_in]
+# AXI Converters
+connect_bd_net [get_bd_pins M2Sconverter/aclk] [get_bd_pins ps/FCLK_CLK0]
+connect_bd_net [get_bd_pins S2Mconverter/aclk] [get_bd_pins ps/FCLK_CLK0]
+connect_bd_net [get_bd_pins S2Mconverter/aresetn] [get_bd_pins system_rst/interconnect_aresetn]
+connect_bd_net [get_bd_pins M2Sconverter/aresetn] [get_bd_pins system_rst/interconnect_aresetn]
 
-# Logger to interconnect
-connect_bd_intf_net -boundary_type upper [get_bd_intf_pins interconnect/M01_AXI] [get_bd_intf_pins logger/S0]
-connect_bd_intf_net [get_bd_intf_pins logger/M0] -boundary_type upper [get_bd_intf_pins interconnect/S00_AXI]
+connect_bd_intf_net [get_bd_intf_pins M2Sconverter/M_AXI] [get_bd_intf_pins ps/S_AXI_HP0]
+connect_bd_intf_net [get_bd_intf_pins M2Sconverter/S_AXI] [get_bd_intf_pins logger/M0]
+
+connect_bd_intf_net [get_bd_intf_pins S2Mconverter/S_AXI] [get_bd_intf_pins ps/M_AXI_GP0]
+connect_bd_intf_net [get_bd_intf_pins S2Mconverter/M_AXI] [get_bd_intf_pins logger/S0]
+
+# Clock to System Reset
+connect_bd_net [get_bd_pins system_rst/slowest_sync_clk] [get_bd_pins ps/FCLK_CLK0]
+connect_bd_net [get_bd_pins system_rst/ext_reset_in] [get_bd_pins ps/FCLK_RESET0_N]
+
+save_bd_design
