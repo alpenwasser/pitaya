344af4782e01e69a400761ce550c3d214c26d6ca
alpenwasser <webmaster@alpenwasser.net>
2017-06-20 09:33:59 +0200

    tcl dispatcher for filter design

--- design/fpga/filters.tcl
+++ design/fpga/filters.tcl
@@ -76,3 +76,4 @@ wait_on_run synth_1
 if {[get_property PROGRESS [get_runs synth_1]] != "100%"} {
     error "ERROR: synth_1 failed"
 }
+close_project
---
+++ design/fpga/vivadoDispatcher.tcl
@@ -0,0 +1,17 @@
+set path [pwd]
+set report_dir $path/reports
+set report_src tmp/filter/filter.runs/synth_1/design_1_wrapper_utilization_synth.rpt
+set decFIR_dir $path/../filter/coefData/decFIR
+
+cd $decFIR_dir
+set coe_files [glob -type f *]
+cd $path
+
+# from firmware/fpga/design_cores.tcl
+foreach coe_file $coe_files {                                                                                                                                                                                                                   
+    set argv "$decFIR_dir/$coe_file"
+    source filters.tcl
+    set report_target decFIR--$coe_file
+    regsub coe $report_target rpt report_target
+    file copy -force $report_src $report_dir/$report_target
+}
