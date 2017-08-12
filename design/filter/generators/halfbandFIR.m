b02fc4663eac55a40fc26f37d9d708239f12a8fd
alpenwasser <webmaster@alpenwasser.net>
2017-08-12 15:46:57 +0200

    halfband filter: bugfix, cliDispatcher: detail passband plots

--- design/filter/generators/halfbandFIR.m
+++ design/filter/generators/halfbandFIR.m
@@ -107,7 +107,7 @@ for l = 0:L-1
         end
 
         % Save Filter Plot Data
-        [H,W] = freqz(Hd{n,4}, 1e3);
+        [H,W] = freqz(Hd{n,1}, 1e3);
         fh = fopen(plotFile, 'w');
         if fh ~= -1
             fprintf(fh, '%s,%s,%s\n', 'abs(H)', 'angle(H)', 'W');
